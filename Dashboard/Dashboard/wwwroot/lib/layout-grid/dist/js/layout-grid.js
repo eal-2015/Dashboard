25135d484c11c07453a241f6888baaea24b6bbe9
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2017-08-15 13:21:05 +0200

    Added example boxes

--- Dashboard/Dashboard/wwwroot/lib/layout-grid/dist/js/layout-grid.js
+++ Dashboard/Dashboard/wwwroot/lib/layout-grid/dist/js/layout-grid.js
@@ -186,33 +186,33 @@ var LTGrid = (function ($) {
     }
 
     var Default = {
-        resize: true,
+        resize: false,
         overlap: false,
-        compact: true,
+        compact: false,
         params: {
             lg: {
                 gap: 1,
                 maxWidth: Number.MAX_VALUE,
                 cols: 4,
-                aspect: 2 / 3
+                aspect: 1
             },
             md: {
                 gap: 2,
                 maxWidth: 1200,
                 cols: 3,
-                aspect: 2 / 3
+                aspect: 1
             },
             sm: {
                 gap: 3,
                 maxWidth: 992,
                 cols: 2,
-                aspect: 2 / 3
+                aspect: 1
             },
             xs: {
                 gap: 4,
                 maxWidth: 768,
                 cols: 1,
-                aspect: 2 / 3
+                aspect: 1
             }
         }
     }
